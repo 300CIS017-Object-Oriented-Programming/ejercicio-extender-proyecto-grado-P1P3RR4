22e50abaded57fa4a181f13718e0e130c219a5dc
Juan Felipe Muñoz <jjfmunoz203@javerianacali.edu.co>
2022-11-06 18:55:44 -0500

    New: nuevo criterio y directores

--- controller/ControladorPDF.py
+++ controller/ControladorPDF.py
@@ -24,20 +24,19 @@ class ControladorPdf:
         self.pdf.cell(200, 10, txt="ACTA DE EVALUACIÓN DE GRADO", ln=1, align='C')
         self.pdf.cell(200, 10, txt="Facultad de Ingeniería.", ln=2, align='C')
         self.pdf.set_font("times", size=12)
-        self.pdf.multi_cell(185, 6,
-                       txt="Trabajo de grado denominado: Análisis de la similitud en el código fuente de dos programas de computador usando técnicas de inteligencia artificial",
-                       align='L')
         for acta in controlador.actas:
             if acta.autor == acta_seleccionada:
                 flag = True
+                self.pdf.multi_cell(185, 6, txt=f"Trabajo de grado:                {acta.nombre_trabajo}", align= 'L')
                 self.pdf.cell(200, 10, txt=f"Autor:                                {acta.autor}", ln=1, align='L')
-                self.pdf.cell(200, 10, txt=f"Fecha/Periodo:                  {acta.fecha_acta} Tesis II", ln=1, align='L')
-                self.pdf.cell(200, 10, txt=f"Director:                            {acta.director}", ln=1, align='L')
-                self.pdf.cell(200, 10, txt=f"Codirector:                        {acta.codirector}", ln=1, align='L')
-                self.pdf.cell(200, 10, txt=f"Nombre Del Trabajo:        {acta.nombre_trabajo}", ln=1, align='L')
-                self.pdf.cell(200, 10, txt=f"Tipo De Trabajo:               {acta.tipo_trabajo}", ln=1, align='L')
-                self.pdf.cell(200, 10, txt=f"Jurado 1:                            {acta.jurado1}", ln=1, align='L')
-                self.pdf.cell(200, 10, txt=f"Jurado 2:                            {acta.jurado2}", ln=1, align='L')
+                self.pdf.cell(200, 10, txt=f"Fecha/Periodo:                        {acta.fecha_acta} Tesis II", ln=1, align='L')
+                self.pdf.cell(200, 10, txt=f"Director:                             {acta.director}", ln=1, align='L')
+                self.pdf.cell(200, 10, txt=f"Codirector:                           {acta.codirector}", ln=1, align='L')
+                self.pdf.cell(200, 10, txt=f"Nombre Del Trabajo:                   {acta.nombre_trabajo}", ln=1, align='L')
+                self.pdf.cell(200, 10, txt=f"Tipo De Trabajo:                      {acta.tipo_trabajo}", ln=1, align='L')
+                self.pdf.cell(200, 10, txt=f"Jurado 1:                             {acta.jurado1}", ln=1, align='L')
+                self.pdf.cell(200, 10, txt=f"Jurado 2:                             {acta.jurado2}", ln=1, align='L')
+                self.pdf.cell(200, 10, txt=f"Fecha de presentacion:                  {acta.fecha_presentacion}", ln=1, align='L')
                 self.pdf.multi_cell(185, 6,
                                txt="En atención al desarrollo de este Trabajo de Grado y al documento y sustentación que presentó el(la) autor(a), los Jurados damos las siguientes calificaciones parciales y observaciones (los criterios a evaluar y sus ponderaciones se estipulan en el artículo 7.1 de las Directrices para Trabajo de Grado de Maestría):",
                                align='L')
@@ -64,7 +63,8 @@ class ControladorPdf:
                 self.pdf.cell(95, 10, txt="Números", ln=0, align='C')
                 self.pdf.cell(35, 10, txt="Letras", ln=1, align='C')
                 self.pdf.set_font("times", size=12)
-                self.pdf.multi_cell(185, 5, txt="Observaciones adicionales: ", align='L')
+                self.pdf.multi_cell(185, 5, txt=f"Observaciones adicionales: {criterio.observacion_ad}", align='L')
+                self.pdf.multi_cell(185, 5, txt= f"Restricciones: {criterio.restriccion}", align='L')
                 self.pdf.multi_cell(185, 5,
                                txt="_____________________________________________________________________________________",
                                align='L')
